9f48b4d5d6d4082a7516aa22ded4fbddab11acdf
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-19 08:38:32 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-        {children}
+          {children}
         </ThemeProvider>
       </body>
     </html>
