1917d365509e0f7d892cf8de056501227af7dc3b
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-21 15:17:56 +0500

    update

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/app/(Home)/navbar"
+import { Footer } from "./footer"
 
 interface LayoutProps {
     children: React.ReactNode
@@ -8,7 +9,10 @@ const Layout = ({children}: LayoutProps ) => {
   return (
     <div className="flex flex-col min-h-screen">
         <Navbar />
+        <div className="flex-1">
       {children}
+        </div>
+        <Footer/>
     </div>
   )
 }
--- src/app/(Home)/navbar-sidebar.tsx
+++ src/app/(Home)/navbar-sidebar.tsx
@@ -1,11 +1,13 @@
-import { 
+import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle
 
- } from "@/components/ui/sheet";
- import { ScrollArea } from "@/components/ui/scroll-area";
+} from "@/components/ui/sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Link from "next/link";
+
 
 interface NavbarItem {
   href: string;
@@ -14,21 +16,46 @@ interface NavbarItem {
 
 interface props {
   item: NavbarItem[];
-  open : boolean;
-  onOpenChange : (open : boolean) => void;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
 }
 
-export const NavbarSidebar = ({item, open, onOpenChange} : props) => {
+export const NavbarSidebar = ({ item, open, onOpenChange }: props) => {
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent side="left" className="w-80">
-        <SheetHeader>
-          <SheetTitle>Menu</SheetTitle>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <div className="flex items-center">
+            <SheetTitle>Menu</SheetTitle>
+          </div>
         </SheetHeader>
-        <ScrollArea className="flex-1">
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2 ">
           {item.map((item) => (
-            <div key={item.href}>{item.children}</div>
+            <Link
+              key={item.href}
+              href={item.href}
+              className="w-full text-left px-4  hover:bg-foreground hover:text-background flex items-center text-base font-medium "
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
           ))}
+          <div className="border-t">
+              <Link 
+              href="/sign-in"
+              className="w-full text-left px-4 hover:bg-foreground hover:text-background flex items-center text-base font-medium "
+              onClick={() => onOpenChange(false)}
+              >
+                Log In
+                </Link>
+              <Link 
+              href="/sign-up"
+              className="w-full text-left px-4 hover:bg-foreground hover:text-background flex items-center text-base font-medium "
+              onClick={() => onOpenChange(false)}
+              >
+                Start Selling
+              </Link>
+          </div>
         </ScrollArea>
       </SheetContent>
     </Sheet>
