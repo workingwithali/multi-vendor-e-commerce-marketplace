6489764a8fa7115b24e2da8a1a0090c0a34f4309
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-30 16:15:28 +0500

    update

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -3,6 +3,7 @@ import { cache } from 'react';
 
 import config from '@payload-config'
 import { getPayload } from 'payload'
+import superjson from 'superjson';
 
 export const createTRPCContext = cache(async () => {
     /**
@@ -18,7 +19,7 @@ const t = initTRPC.create({
     /**
      * @see https://trpc.io/docs/server/data-transformers
      */
-    // transformer: superjson,
+    transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
