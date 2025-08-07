334d6d6d3f94a1b882711d0b97468527616072cc
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-08-07 16:15:03 +0500

    update

--- src/app/(app)/(Home)/[category]/page.tsx
+++ src/app/(app)/(Home)/[category]/page.tsx
@@ -23,9 +23,7 @@ const Page = async ({ params }: Props) => {
       <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
         <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12 ">
           <div className="lg:col-span-2 xl:col-span-2">
-            <div className="p-2">
               <ProductFilters />
-            </div>
           </div>
           <div className="lg:col-span-4 xl:col-span-6">
             <Suspense fallback={<ProductListSkeleton />}>
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -15,16 +15,21 @@ export const productsRouter = createTRPCRouter({
         )
         .query(async ({ ctx, input }) => {
             const where: Where = {};
-            if(input.minPrice){
+            if(input.minPrice && input.maxPrice){
+                where.price = {
+                    less_than_equal: input.maxPrice,
+                    greater_than_equal: input.minPrice
+                }
+            }else if(input.minPrice){
                 where.price = {
                     greater_than_equal: input.minPrice
                 }
-            }
-            if(input.maxPrice){
+            }else if(input.maxPrice){
                 where.price = {
                     less_than_equal: input.maxPrice
                 }
             }
+            
 
 
             if (input.category) {
