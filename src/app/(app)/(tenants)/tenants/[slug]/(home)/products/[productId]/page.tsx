8d3c8f7af172c08cb025b055360b99a4cae83ea8
workingwithali <alirashid2020e@gmail.com>
2025-08-18 12:50:32 +0500

    update

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -1,13 +1,27 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 
-interface Props {}
+interface Props {
+    params: Promise<{
+    slug: string;
+    productId: string
+  }>,
+}
 
 
 
-const ProductPage = () => {
+const Page = async( { params }: Props) => {
+    const { productId , slug } = await params;
+    const queryClient = getQueryClient();
+      void queryClient.prefetchQuery(trpc.tenants.getOne.queryOptions({
+        slug,
+      }));
   return (
-    <div>ProductPage</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+        <ProductView slug={slug} productId={productId} />
+    </HydrationBoundary>
   )
 }
 
-export default ProductPage
+export default Page
