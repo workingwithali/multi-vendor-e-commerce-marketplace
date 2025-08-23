7091d06f041de6b2da914bddc792daef181ebf49
workingwithali <alirashid2020e@gmail.com>
2025-08-23 11:43:41 +0500

    update

--- src/modules/checkout/ui/components/checkout-item.tsx
+++ src/modules/checkout/ui/components/checkout-item.tsx
@@ -1,11 +1,22 @@
 interface Props {
-  
+  isLast?: boolean;
+  id: string;
+  name: string;
+  tenantName: string;
+  imageUrl?: string;
+  tenantUrl: string;
+  productUrl: string;
+  price: number;
+  onRemove: () => void
 }
 
+import { cn } from '@/lib/utils';
 import React from 'react'
 
-export const CheckoutItems = () => {
+export const CheckoutItems = ( { isLast, id, name, tenantName, imageUrl, tenantUrl, productUrl, price, onRemove }: Props) => {
   return (
-    <div>CheckoutItems</div>
+    <div className={cn("grid grid-cols-[8.5rem_1fr_auto] gap-4 pr-4 border-b", isLast && "border-b-0")}>
+      CheckoutItems
+    </div>
   )
 }
