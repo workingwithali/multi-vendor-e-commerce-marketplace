078246f42d916aa6723195013a6bf3ffc9076e0c
workingwithali <alirashid2020e@gmail.com>
2025-09-03 21:03:02 +0500

    update

--- src/modules/library/ui/components/review-form.tsx
+++ src/modules/library/ui/components/review-form.tsx
@@ -5,19 +5,90 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { formSchema } from "@/modules/reviews/schema";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage
+} from "@/components/ui/form";
+import { init } from "next/dist/compiled/webpack/webpack";
 
 
-interface Props{
+interface Props {
     productId: string,
     initialData?: ReviewGetOneOutput
 }
 export const ReviewForm = ({ productId, initialData }: Props) => {
+    const [isPreview, setIsPreview] = useState(!!initialData);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            rating: initialData?.rating ?? 0,
+            comment: initialData?.comment ?? "",
+        },
+    })
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        console.log(data);
+    }
     return (
-        <div>
-            reviews types
-        </div>
+        <Form {...form}>
+            <form
+                className="flex flex-col gap-4"
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <p>
+                    {isPreview ? "Preview" : "Liked it? Give us a review!"}
+                </p>
+                <FormField
+                    control={form.control}
+                    name="comment"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Textarea
+                                    placeholder="Write your review here..."
+                                    disabled={isPreview}
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                {!isPreview && (
+
+                    <Button
+                        variant="ghostOutline"
+                        disabled={false}
+                        type="submit"
+                        size="lg"
+                        className="bg-foreground text-background hover:bg-primary hover:text-foreground w-fit"
+
+                    >
+                {initialData ? "Updata review" : "Post review"}
+                    </Button>
+                )}
+                {isPreview && (
+                    <Button
+                     onClick={() => setIsPreview(false)}
+                     variant="ghostOutline"
+                     disabled={false}
+                     type="button"
+                     size="lg"
+                     className="w-fit border border-foreground hover:bg-foreground hover:text-background"
+                     >
+                        Edit
+                    </Button>
+                )}
+
+            </form>
+        </Form>
     )
 }
