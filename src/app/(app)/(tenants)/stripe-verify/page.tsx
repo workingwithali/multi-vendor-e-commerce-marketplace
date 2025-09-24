11cec21467d313e2222ae286050f8c32d3031a95
workingwithali <alirashid2020e@gmail.com>
2025-09-24 19:00:08 +0500

    update

---
+++ src/app/(app)/(tenants)/stripe-verify/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { LoaderIcon } from "lucide-react";
+import { useEffect } from "react";
+
+
+const page = () => {
+  const trpc = useTRPC();
+  const {mutate: verify} = useMutation(trpc.checkout.verify.mutationOptions({
+    onSuccess(data) {
+      window.location.href = data.url;
+    },
+    onError(error) {
+      window.location.href = '/';
+    }
+  }));
+  useEffect(() => {
+    verify();
+  }, [verify])
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <LoaderIcon className="animate-spin size-4 text-muted-foreground" />
+    </div>
+  )
+}
+
+export default page
